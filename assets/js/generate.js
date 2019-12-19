f7abc41989fb1d51d2cee127fb45633f1a428f04
ramiforde <ramiforde@gmail.com>
2019-12-19 14:33:53 -0500

    I had to redo my js page to allow the user to choose whether they would like numbers or symbols with checkbox, then added a copy to clipboard function to allow the user to copy password.

--- assets/js/generate.js
+++ assets/js/generate.js
@@ -1,35 +1,88 @@
-// // Alert prompt to allow user choose which type of password they would like 
-// var specChar = prompt("Would you like to have special characters in your password? Select 'y' for yes and 'n' for no.");
-// var upCase = prompt("Would you like to have Uppercase characters in your password? Select 'y' for yes and 'n' for no.");
-// var lowCase = prompt("Would you like to have Lowercase characters in your password? Select 'y' for yes and 'n' for no.");
-// // Give values to each character based on user prompt
-// var specValues = "1234567890!@#$%^&*()";
-// var upperValues = "ABCDEFGHIGKLMNOP";
-// var lowerValues = "abcdefghigklmnop";
+// Get all of the element used with js for var
+var char = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+var num = "123456789";
+var symbol = "!@#$%^&*()_:><?/";
 
-// Assign value to user prompt based on true/fa
 
- // Password length/complexity
- function generate(){
- var length = document.getElementById("slider-length").value;
- var values ="ABCDEFGHIGKLMNOPabcdefghigklmnop1234567890!@#$%^&*()";
- var password="";
 
-//  Created loop to choose passwords characters
- for (i = 0;i <= length; i++){
-     password = password + values.charAt(Math.floor(Math.random() * Math.floor(values.length - 1)));
+var generate = document.getElementById("generate");
+var numBox= document.getElementById("number");
+var symbolBox = document.getElementById("symbol");
+var yourPassword = document.getElementById("display");
+
+
+
+generate.addEventListener("click", function(e){
+   yourPassword.value = password(numBox.value, blank);
+}
+);
+
+function password(length,characters){
+   var pwd = "";
+
+   for(i = 0; i < length; i ++){
+      pwd += characters.charAt(Math.floor(Math.random() * characters.length));
+   }
+
+   return pwd;
 }
 
-// add password to text-box
-document.getElementById("display").value = password;
-
-// add length div set value of 25
- document.getElementById("length").innerHTML = function slider(){
-    //  var length = 
+function copyFunction() {
+   /* Get the text field */
+   var copyText = document.getElementById("display");
+ 
+   /* Select the text field */
+   copyText.select();
+   copyText.setSelectionRange(0, 99999); /*For mobile devices*/
+ 
+   /* Copy the text inside the text field */
+   document.execCommand("copy");
+ 
+   /* Alert the copied text */
+   alert("Copied the text: " + copyText.value);
  }
+ 
 
- }
 
- function copyText(){
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//  function generate(){
+//  var length = document.getElementById("slider-length").value;
+//  var values ="ABCDEFGHIGKLMNOPabcdefghigklmnop1234567890!@#$%^&*()";
+//  var password="";
+
+// //  Created loop to choose passwords characters
+//  for (i = 0;i <= length; i++){
+//      password = password + values.charAt(Math.floor(Math.random() * Math.floor(values.length - 1)));
+// }
+
+// // add password to text-box
+// document.getElementById("display").value = password;
+
+// // add length div set value of 25
+//  document.getElementById("length").innerHTML = function slider(){
+//     //  var length = 
+//  }
+
+//  }
+
+//  function copyText(){
      
- }
+//  }
---
+++ psuedocode.md
@@ -0,0 +1,3 @@
+1. Get all of my elements/id/class for var in js
+2. Want to prompt user via 'alert' to choose between 'y' for yes or 'n' for no out of 3 catorgories (special character, uppercase characters, lower case characters).
+<!-- Unfinished -->
